73d8b5e5811435bd4b308d28e9454cb8455d7b16
kiprotichdominic <kiprotichdominic102@gmail.com>
2019-12-18 05:35:02 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from app import app
+from .request import get_movies
+
+# Views
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title=title, popular=popular_movies)
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html', id=movie_id)
